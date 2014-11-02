a3f04d1edcf449a91d53553e323587256475d299
sanemat <o.gata.ken@gmail.com>
2014-11-02 19:47:53 +0900

    Update

--- langturn.com-translations-33/build.sh
+++ langturn.com-translations-33/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cp html5github.html5 ~/.pandoc/templates/.
-~/.cabal/bin/pandoc -f markdown -t html5 index.md -s -o index.html
+~/.cabal/bin/pandoc --template=html5github -f markdown -t html5 index.md -s -o index.html
